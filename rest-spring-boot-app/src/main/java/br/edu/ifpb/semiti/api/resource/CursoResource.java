bee355e458009bdf32ecc2326fa0db31b875c76f
André Luís <tr.andreluis@gmail.com>
2019-10-11 15:11:05 -0300

    wip: implementacao do cadastro de um Curso

--- rest-spring-boot-app/src/main/java/br/edu/ifpb/semiti/api/resource/CursoResource.java
+++ rest-spring-boot-app/src/main/java/br/edu/ifpb/semiti/api/resource/CursoResource.java
@@ -3,14 +3,28 @@ package br.edu.ifpb.semiti.api.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifpb.semiti.api.model.Curso;
+import br.edu.ifpb.semiti.api.service.CursoService;
 
 @RestController("/cursos")
 public class CursoResource {
+
+	@Autowired
+	private CursoService cursoService;
+
+	@PostMapping
+	public ResponseEntity<Curso> salvar(@RequestBody Curso curso) {
+		Curso cursoSalvo = cursoService.salvar(curso);
+		return ResponseEntity.status(HttpStatus.CREATED).body(cursoSalvo);
+	}
 
 	@GetMapping
 	public ResponseEntity<List<Curso>> listar() {
---
+++ rest-spring-boot-app/src/main/java/br/edu/ifpb/semiti/api/service/CursoService.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.semiti.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.ifpb.semiti.api.model.Curso;
+import br.edu.ifpb.semiti.api.repository.CursoRepository;
+
+@Service
+public class CursoService {
+
+	@Autowired
+	private CursoRepository cursoRepository; 
+	
+	public Curso salvar(Curso curso) {
+		return cursoRepository.save(curso);
+	}
+	
+}
